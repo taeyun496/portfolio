af8709c4daec4f53f7bd9c4c3d8c182ec5031694
taeyun496 <lty32325@gmail.com>
2025-04-05 13:01:26 +0900

    fix: update build path and add homepage

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+}
+
+module.exports = nextConfig 
